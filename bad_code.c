b045940b3a26fb95b429b6cd1fb050d43c6efff6
Minnie Mu <68094429+Minniemu@users.noreply.github.com>
2023-05-26 15:42:50 +0800

    Add files via upload

---
+++ bad_code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {   
+    int number;
+   
+    printf("Enter an integer: ");  
+    int a;
+    a = 0
+    // reads and stores input
+    scanf("%d", &number);
+
+    // displays output
+    printf("You entered: %d", number);
+    
+    return 0;
+}
